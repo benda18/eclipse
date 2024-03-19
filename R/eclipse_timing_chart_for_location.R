fbfbadff191bf0374e7e63481c82ae9e5f1582ad
Tim Bender <bendertj@gmail.com>
2024-03-19 12:22:58 -0400

    working script, but un-unified formulas

--- R/eclipse_timing_chart_for_location.R
+++ R/eclipse_timing_chart_for_location.R
@@ -118,7 +118,31 @@ ec_sched <- function(lon_in, lat_in, time_ny){
   return(out.times)
 }
 
-ec_sched(lon, lat, time_NY)
+ecsched.times <- ec_sched(lon, lat, time_NY)
 
+
+# get sun coverage----
+
+# convert time to julian
+ecsuncov <- NULL
+
+for(i in 1:length(ecsched.times)){
+  i_time <-  swephR::swe_julday(year = year(with_tz(ecsched.times[i],"UTC")), 
+                                month = lubridate::month(with_tz(ecsched.times[i],"UTC")), 
+                                day = mday(with_tz(ecsched.times[i],"UTC")), 
+                                hourd = lubridate::hour(with_tz(ecsched.times[i],"UTC")) + 
+                                  (lubridate::minute(with_tz(ecsched.times[i],"UTC"))/60), 
+                                gregflag = 1)
+  
+  ecsuncov <- c(ecsuncov, 
+                swephR::swe_sol_eclipse_how(jd_ut = i_time, 
+                                            ephe_flag = 4, 
+                                            geopos = c(x = lon, 
+                                                       y = lat, 
+                                                       z = 10))$attr[1])
+}
+
+data.frame(time = ecsched.times, 
+           coverage = ecsuncov) 
 
 
