c529ceabf3f1370478c38c4927058bb5f07441c0
Tim Bender <bendertj@gmail.com>
2024-03-23 09:24:16 -0400

    commit

--- R/next eclipse2.R
+++ R/next eclipse2.R
@@ -7,8 +7,8 @@ library(scales)
 library(ggplot2)
 library(sf)
 library(renv)
-library(rnaturalearth)
-library(rnaturalearthdata)
+#library(rnaturalearth)
+#library(rnaturalearthdata)
 
 
 getwd()
@@ -51,20 +51,56 @@ a.date.ju <- swephR::swe_utc_to_jd(year = year(start.date),
                                    sec   = 0, 
                                    gregflag = 1)$dret[2]
 
-when_next <- swe_sol_eclipse_when_loc(jd_start = a.date.ju, 
-                                      ephe_flag = 4, 
-                                      geopos = c(x = var.lon, 
-                                                 y = var.lat, 
-                                                 z = 10), 
-                                      backward = F)
+out.df       <- NULL
+is_total_ecl <- F
+n <- 0
+while(!is_total_ecl){
+  n <- n + 1
+  if(n > 1000){
+    stop("broken")
+  }
+  
+  if(n > 1){
+    a.date.ju <- when_next$tret[1]+1
+  }
+  
+  when_next <- swe_sol_eclipse_when_loc(jd_start = a.date.ju, 
+                                        ephe_flag = 4, 
+                                        geopos = c(x = var.lon, 
+                                                   y = var.lat, 
+                                                   z = 10), 
+                                        backward = F)
+  
+  
+  temp.nextdate <- when_next$tret[1] %>% # time of maximum eclipse 
+    swephR::swe_jdet_to_utc(., 1) %>%
+    paste(., sep = "-", collapse = "-") %>%
+    ymd_hms()
+  
+  temp.nextobs <- when_next$attr[1] # pct obscred
+  
+  # is partial or total? 
+  
+  if(n == 1 | 
+     ifelse(temp.nextobs < 1, 
+            "partial", "total") == "total"){
+    out.df <- rbind(out.df, 
+                    data.frame(from_date = start.date,
+                               nth_ecl   = n,
+                               lon = var.lon,
+                               lat = var.lat,
+                               ecl_date = temp.nextdate, 
+                               ecl_obsc = temp.nextobs, 
+                               ecl_type = ifelse(temp.nextobs < 1, 
+                                                 "partial", "total")))
+  }
+  
+  if(ifelse(temp.nextobs < 1, 
+            "partial", "total") == "total"){
+    is_total_ecl <- T
+  }
+  
+}
 
-temp.nextdate <- when_next$tret[1] %>% # time of maximum eclipse 
-  swephR::swe_jdet_to_utc(., 1) %>%
-  paste(., sep = "-", collapse = "-") %>%
-  ymd_hms()
+out.df
 
-temp.nextobs <- when_next$attr[1] # pct obscred
-
-
-data.frame(ecl_date = temp.nextdate, 
-           ecl_obsc = temp.nextobs)
