de2de74a274fb5ed635201232dea668cf9f1f570
Tim Bender <bendertj@gmail.com>
2024-03-25 14:10:11 -0400

    working future lunar eclipse

--- R/next eclipse2.R
+++ R/next eclipse2.R
@@ -15,13 +15,14 @@ getwd()
 
 renv::snapshot()
 renv::status()
-rm(list=ls()[ls() != "earth.coast"]);cat('\f')
+rm(list=ls()[ls() != "get.addr"]);cat('\f')
 
 
 # vars----
 
-
-get.addr <- censusxy::cxy_oneline(address = "7318 Overland Park Court, west chester, oh")
+if(! "get.addr" %in% ls()){
+  get.addr <- censusxy::cxy_oneline(address = "1447 newcastle rd, durham nc")
+}
 
 var.lon <- unlist(unname(get.addr["coordinates.x"]))
 var.lat <- unlist(unname(get.addr["coordinates.y"]))
@@ -71,6 +72,66 @@ if(temp.nextobs < 1 &
 }else{
   next.total.eclipse <-  strftime(start.date, format = "%B %d, %Y")
 }
+
+
+
+# next lunar eclipse----
+le_type <- function(mag_u){
+  # total 
+  tot_ecl <- mag_u >= 1
+  # penumbral
+  pen_ecl <- mag_u < 0
+  # partial
+  par_ecl <- !xor(tot_ecl,pen_ecl)
+  
+  out <- c("Total Lunar" = tot_ecl, 
+           "Penumbral Lunar" = pen_ecl, 
+           "Partial Lunar" = par_ecl)
+  
+  out <- out[out == T]
+  out <- names(out)
+  return(out)
+}
+
+start.date <- ymd(20240409)
+
+if(! "get.addr" %in% ls()){
+  get.addr <- censusxy::cxy_oneline(address = "1447 newcastle rd, durham nc")
+}
+
+var.lon <- unlist(unname(get.addr["coordinates.x"]))
+var.lat <- unlist(unname(get.addr["coordinates.y"]))
+
+a.date.ju <- swephR::swe_utc_to_jd(year = year(start.date), 
+                                   month = lubridate::month(start.date), 
+                                   day   = mday(start.date), 
+                                   houri = 0, 
+                                   min   = 30, 
+                                   sec   = 0, 
+                                   gregflag = 1)$dret[2]
+
+when_lunar <- swephR::swe_lun_eclipse_when_loc(jd_start = a.date.ju, 
+                                               ephe_flag = 4, 
+                                               geopos = c(x = var.lon, 
+                                                          y = var.lat, 
+                                                          z = 10), 
+                                               backward = F)
+
+# https://www.astro.com/swisseph/swephprg.htm#_Toc112948992
+
+next_lunar <- strftime(x = ymd_hms(paste(swephR::swe_jdet_to_utc(when_lunar$tret[1], 1), 
+                                         sep = "-", collapse = "-")), 
+                       format = "%B %d, %Y at %I:%M%p %Z")
+app_alt_deg <- when_lunar$attr[7]
+
+ecl_type <- le_type(when_lunar$attr[1])
+
+data.frame(date_time = next_lunar, 
+           type = ecl_type, 
+           degrees_above_horizon = round(app_alt_deg,1))
+
+#mag_u <- mag.umb  <- when_lunar$attr[1]
+
 
 
 
--- shiny_next_eclipse/app.R
+++ shiny_next_eclipse/app.R
@@ -58,8 +58,12 @@ ui <- fluidPage(
     mainPanel(
       wellPanel(
         fluidRow("title"), 
-        fluidRow(tableOutput(outputId = "return_nextSOL"))
-        
+        wellPanel(
+          fluidRow(tableOutput(outputId = "return_nextSOL"))  
+        ),
+        wellPanel(
+          fluidRow(tableOutput(outputId = "return_nextLUN"))
+        ),
       )
     )
   )
@@ -67,8 +71,63 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  le_type <- function(mag_u){
+    # total 
+    tot_ecl <- mag_u >= 1
+    # penumbral
+    pen_ecl <- mag_u < 0
+    # partial
+    par_ecl <- !xor(tot_ecl,pen_ecl)
+    
+    out <- c("Total Lunar" = tot_ecl, 
+             "Penumbral Lunar" = pen_ecl, 
+             "Partial Lunar" = par_ecl)
+    
+    out <- out[out == T]
+    out <- names(out)
+    return(out)
+  }
   get_cxyinfo <- eventReactive(input$cxy_go, {
     censusxy::cxy_oneline(address = input$addr_in)
+  })
+  
+  get_nextLUN <- eventReactive(eventExpr = input$cxy_go, {{
+    start.date <- input$in_startdate
+    get.addr   <- get_cxyinfo()
+    var.lon    <- unlist(unname(get.addr["coordinates.x"])) # -78.9
+    var.lat    <- unlist(unname(get.addr["coordinates.y"])) #  36.0
+    
+    a.date.ju <- swephR::swe_utc_to_jd(year = year(start.date), 
+                                       month = lubridate::month(start.date), 
+                                       day   = mday(start.date), 
+                                       houri = 0, 
+                                       min   = 30, 
+                                       sec   = 0, 
+                                       gregflag = 1)$dret[2]
+    
+    when_lunar <- swephR::swe_lun_eclipse_when_loc(jd_start = a.date.ju, 
+                                                   ephe_flag = 4, 
+                                                   geopos = c(x = var.lon, 
+                                                              y = var.lat, 
+                                                              z = 10), 
+                                                   backward = F)
+    
+    # https://www.astro.com/swisseph/swephprg.htm#_Toc112948992
+    
+    next_lunar <- strftime(x = ymd_hms(paste(swephR::swe_jdet_to_utc(when_lunar$tret[1], 1), 
+                                             sep = "-", collapse = "-")), 
+                           format = "%B %d, %Y at %I:%M%p %Z")
+    app_alt_deg <- when_lunar$attr[7]
+    
+    ecl_type <- le_type(when_lunar$attr[1])
+    
+    data.frame(date_time = next_lunar, 
+               type = ecl_type, 
+               degrees_above_horizon = round(app_alt_deg,1))
+    
+  }})
+  output$return_nextLUN <- renderTable({
+    get_nextLUN()
   })
   
   get_nextSOL <- eventReactive(eventExpr = input$cxy_go, {
@@ -97,13 +156,11 @@ server <- function(input, output) {
                                                  sep = "-", collapse = "-"))), 
                            format = "%B %d, %Y")
       ecl_type <- ifelse(when_next$attr[2] >= 1, "Total Solar", "Annular Solar")
-        ecl_obs  <- when_next$attr[3]
-        
-        df_ecl <- data.frame(date = ecl_date, 
-                                   type = ecl_type, 
-                                   obscuration = scales::percent(floor(ifelse(ecl_obs >=1, 1, ecl_obs)*100)/100))
+      ecl_obs  <- when_next$attr[3]
       
-     
+      df_ecl <- data.frame(date = ecl_date, 
+                           type = ecl_type, 
+                           obscuration = scales::percent(floor(ifelse(ecl_obs >=1, 1, ecl_obs)*100)/100))
   })
   
   output$return_nextSOL <- renderTable({
