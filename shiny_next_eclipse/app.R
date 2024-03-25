073eb736ec77aa2f855a95e2b3bb9e8fb254ed31
Tim Bender <bendertj@gmail.com>
2024-03-25 13:04:37 -0400

    code cleanup

--- shiny_next_eclipse/app.R
+++ shiny_next_eclipse/app.R
@@ -45,10 +45,6 @@ ui <- fluidPage(
       #                     choices = list("At Address" = 1, 
       #                                    "~1 hour drive" = 0.99, 
       #                                    "~2 hour drive" = 0.98)),
-      # shiny::radioButtons(inputId = "radio_bw", 
-      #                     label = "Search for Future or Past Eclipse?", 
-      #                     choices = list("Future Eclipse(s)" = F,  
-      #                                    "Past Eclipse(s)" = T)),
       shiny::dateInput(inputId = "in_startdate", 
                        label = "Search From Date", 
                        value = Sys.Date(), 
@@ -62,7 +58,6 @@ ui <- fluidPage(
     mainPanel(
       wellPanel(
         fluidRow("title"), 
-        #fluidRow(textOutput(outputId = "return_nextSOL")),
         fluidRow(tableOutput(outputId = "return_nextSOL"))
         
       )
@@ -83,12 +78,7 @@ server <- function(input, output) {
     var.lat <- unlist(unname(get.addr["coordinates.y"])) #  36.0
     
     
-    #df_ecl <- NULL
-    #ecl_found <- F
-    #for(i in 1:10){ # just look for the next 5 
-    #while(start.date <= ymd(21500101)){
-    #while(!ecl_found){
-      a.date.ju <- swephR::swe_utc_to_jd(year = year(start.date), 
+    a.date.ju <- swephR::swe_utc_to_jd(year = year(start.date), 
                                          month = lubridate::month(start.date), 
                                          day   = mday(start.date), 
                                          houri = 0, 
@@ -106,26 +96,16 @@ server <- function(input, output) {
       ecl_date <- strftime(as_date(ymd_hms(paste(swephR::swe_jdet_to_utc(when_next$tret[1], 1), 
                                                  sep = "-", collapse = "-"))), 
                            format = "%B %d, %Y")
-      ## filter for obscuration
-      #if(when_next$attr[3] >= input$radio_obsc){
-        ecl_type <- ifelse(when_next$attr[2] >= 1, "Total Solar", "Annular Solar")
+      ecl_type <- ifelse(when_next$attr[2] >= 1, "Total Solar", "Annular Solar")
         ecl_obs  <- when_next$attr[3]
         
         df_ecl <- data.frame(date = ecl_date, 
                                    type = ecl_type, 
                                    obscuration = scales::percent(floor(ifelse(ecl_obs >=1, 1, ecl_obs)*100)/100))
-      #}
-      #start.date <- ecl_date + days(2)
-      #}
-    
-    
-      #df_ecl$date <- as_date(df_ecl$date)
+      
      
   })
   
-  # output$return_nextSOL <- renderText({
-  #   get_nextSOL()
-  # })
   output$return_nextSOL <- renderTable({
     get_nextSOL()
   })
