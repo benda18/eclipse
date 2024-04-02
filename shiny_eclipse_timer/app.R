1696f27ddd295f758290e495dd6643575e264587
Tim Bender <bendertj@gmail.com>
2024-04-01 23:10:18 -0400

    cleanup

--- shiny_eclipse_timer/app.R
+++ shiny_eclipse_timer/app.R
@@ -120,14 +120,14 @@ server <- function(input, output) {
   
   
   # funs----
-  plot_jpeg <- function(path, add=FALSE){
-    require('jpeg')
-    jpg = readJPEG(path, native=T) # read the file
-    res = dim(jpg)[2:1] # get the resolution, [x, y]
-    if (!add) # initialize an empty plot area if add==FALSE
-      plot(1,1,xlim=c(1,res[1]),ylim=c(1,res[2]),asp=1,type='n',xaxs='i',yaxs='i',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
-    rasterImage(jpg,1,1,res[1],res[2])
-  }
+  # plot_jpeg <- function(path, add=FALSE){
+  #   require('jpeg')
+  #   jpg = readJPEG(path, native=T) # read the file
+  #   res = dim(jpg)[2:1] # get the resolution, [x, y]
+  #   if (!add) # initialize an empty plot area if add==FALSE
+  #     plot(1,1,xlim=c(1,res[1]),ylim=c(1,res[2]),asp=1,type='n',xaxs='i',yaxs='i',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
+  #   rasterImage(jpg,1,1,res[1],res[2])
+  # }
   
   ec_sched <- function(lon_in, lat_in, time_ny){
     # time logic
