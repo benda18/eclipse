77344fb8981084c83bf1b79acb3bef6df5b728eb
Tim Bender <bendertj@gmail.com>
2024-04-04 18:48:11 -0400

    empty app

--- shiny_all_eclipses/app.R
+++ shiny_all_eclipses/app.R
@@ -1,7 +1,5 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
+# This is a Shiny web application.
 # Find out more about building applications with Shiny here:
 #
 #    https://shiny.posit.co/
@@ -13,21 +11,17 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("<title>"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            fluidRow("firstrow")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          fluidRow("FirstRow")
         )
     )
 )
@@ -35,16 +29,8 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    
+    
 }
 
 # Run the application 
