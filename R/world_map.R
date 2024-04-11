eafac6eef73caf6a975f139a6244819fff90e128
Tim Bender <bendertj@gmail.com>
2024-04-10 21:01:58 -0400

    world map

--- R/world_map.R
+++ R/world_map.R
@@ -13,3 +13,25 @@ library(glue)
 #library(rsconnect)
 #library(qrcode)
 library(rnaturalearthdata) # for map of world
+
+
+# renv::snapshot()
+
+rm(list=ls());cat('\f')
+
+
+data("countries110")
+
+data(package = "rnaturalearthdata")
+
+countries110$geounit
+View(countries110)
+countries110$scalerank %>% table
+
+countries110$continent
+
+ggplot() + 
+  geom_sf(data = countries110, 
+          aes(fill = continent), color = "black")+
+  theme(panel.background = element_rect(fill = "#9ce4ff"), 
+        legend.position = "none")
