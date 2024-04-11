b26e7e70a92e61fcf9bee12eefc6a15f1fd5b110
Tim Bender <bendertj@gmail.com>
2024-04-11 18:36:30 -0400

    map working

--- shiny_all_eclipsesV2/app.R
+++ shiny_all_eclipsesV2/app.R
@@ -1,24 +1,50 @@
 
 library(shiny)
+library(leaflet)
+library(geoloc)
+library(swephR)
+library(lubridate)
+library(dplyr)
+library(renv)
+library(leaflet)
 
+#renv::snapshot()
 
-ui <- fluidPage(
-    titlePanel("<title>"),
-    sidebarLayout(
-        sidebarPanel(
-            fluidRow("sidebarPanel")
-        ),
-
-        mainPanel(
-           fluidRow("mainPanel")
-        )
-    )
+ui <- navbarPage(title = "<Title>", 
+                 id = "tabs",
+                 tabPanel("Search by Current Location", 
+                          h2("Where Am I?"),
+                          tags$p("Click the button to get your location"),
+                          geoloc::button_geoloc("myBtn", "Get my Location"),
+                          tags$br(),
+                          leafletOutput("map", 
+                                        width = "50%"),
+                          # wellPanel(
+                          #   fluidRow(
+                          #     textOutput(outputId = "lon_id")
+                          #   ), 
+                          #   fluidRow(
+                          #     textOutput(outputId = "lat_id")
+                          #   )
+                          # )
+                 )
+  
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-   
+  
+  
+  
+  output$map <- renderLeaflet({
+    req(input$myBtn_lon)
+    req(input$myBtn_lat)
+    leaflet() %>%
+      addTiles() %>%
+      setView(as.numeric(input$myBtn_lon), as.numeric(input$myBtn_lat), zoom = 17) %>%
+      addMarkers(as.numeric(input$myBtn_lon), as.numeric(input$myBtn_lat), label = "You're here!")
+  })
+  
 }
 
 # Run the application 
