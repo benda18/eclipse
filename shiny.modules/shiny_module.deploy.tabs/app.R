d0ec0f940557c07a89b8dc0b591981c98a3ee36c
Tim Bender <bendertj@gmail.com>
2024-04-11 15:14:00 -0400

    periodic commit

--- shiny.modules/shiny_module.deploy.tabs/app.R
+++ shiny.modules/shiny_module.deploy.tabs/app.R
@@ -1,39 +1,62 @@
 library(shiny)
 library(renv)
+library(ggplot2)
+library(rnaturalearthdata)
+library(sf)
 
-ui <- navbarPage("Navbar page", id = "tabs",
-                 tabPanel("Home",
-                          actionButton("hideTab", "Hide 'Foo' tab"),
-                          actionButton("showTab", "Show 'Foo' tab"),
-                          actionButton("hideMenu", "Hide 'More' navbarMenu"),
-                          actionButton("showMenu", "Show 'More' navbarMenu")
+ui <- navbarPage(title = "<Title>", 
+                 id = "tabs",
+                 tabPanel(title = "Search by Address (USA only)",
+                          shiny::textInput(inputId = "cxy_in", 
+                                           label = "address")),
+                 tabPanel(
+                   title = "Search by Map Click", 
+                   shiny::plotOutput(outputId = "world_map",
+                                     click = clickOpts(id = "plot_click")), 
+                   wellPanel(
+                     fluidRow(
+                       textOutput(outputId = "lon_id")
+                     ), 
+                     fluidRow(
+                       textOutput(outputId = "lat_id")
+                     )
+                   )
                  ),
-                 tabPanel("Foo", "This is the foo tab"),
-                 tabPanel("Bar", "This is the bar tab"),
-                 navbarMenu("More",
-                            tabPanel("Table", "Table page"),
-                            tabPanel("About", "About page"),
-                            "------",
-                            "Even more!",
-                            tabPanel("Email", "Email page")
+                 tabPanel("Search by Current Location", "This is the bar tab"),
+                 tabPanel("Search by Lon/Lat"),
+                 # navbarMenu("More",
+                 #            tabPanel("Table", "Table page"),
+                 #            tabPanel("About", "About page"),
+                 #            "------",
+                 #            "Even more!",
+                 #            tabPanel("Email", "Email page")
+                 # )
+                 mainPanel(
+                   
                  )
 )
 
+
 server <- function(input, output, session) {
-  observeEvent(input$hideTab, {
-    hideTab(inputId = "tabs", target = "Foo")
+  data("countries50")
+  
+  # print lon and lat inputs----
+  output$lon_id <- renderText({
+    unlist(input$plot_click["x"])
+  })
+  output$lat_id <- renderText({
+    unlist(input$plot_click["y"])
   })
   
-  observeEvent(input$showTab, {
-    showTab(inputId = "tabs", target = "Foo")
-  })
-  
-  observeEvent(input$hideMenu, {
-    hideTab(inputId = "tabs", target = "More")
-  })
-  
-  observeEvent(input$showMenu, {
-    showTab(inputId = "tabs", target = "More")
+  output$world_map <- renderPlot({
+    ggplot() + 
+      geom_sf(data = countries50,#[countries50$continent %in% input$f_continent,],
+              #aes(fill = continent), 
+              fill = "white",
+              color = "#363838") +
+      theme(panel.background = element_rect(fill = "#9ce4ff"), 
+            legend.position = "none", 
+            panel.grid = element_blank())
   })
 }
 
