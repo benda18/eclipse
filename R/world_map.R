2c27553b4921b772e58a71e2ec49b740e42b36bf
Tim Bender <bendertj@gmail.com>
2024-04-10 23:12:12 -0400

    filter by country

--- R/world_map.R
+++ R/world_map.R
@@ -31,7 +31,14 @@ countries110$scalerank %>% table
 countries110$continent
 
 ggplot() + 
-  geom_sf(data = countries110, 
-          aes(fill = continent), color = "black")+
+  geom_sf(data = countries110[countries110$continent %in% 
+                                c("North America", 
+                                  "South America"),], 
+          aes(fill = continent))+
   theme(panel.background = element_rect(fill = "#9ce4ff"), 
         legend.position = "none")
+
+
+sort(unique(countries110$geounit[countries110$continent == "Oceania"]) ) %>%
+  paste(collapse = "\", \"") %>%
+  cat()
--- shiny.module_worldmap/app.R
+++ shiny.module_worldmap/app.R
@@ -4,7 +4,7 @@
 #
 # Find out more about building applications with Shiny here:
 #
-#    https://shiny.posit.co/
+# https://shiny.posit.co/
 #
 
 library(shiny)
@@ -26,25 +26,75 @@ library(rnaturalearthdata)
 
 # Define UI for application 
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("title"),
-
-    # Sidebar 
-    sidebarLayout(
-        sidebarPanel(
-            
-        ),
-        mainPanel(
-          
-        )
+  
+  # Application title
+  titlePanel("title"),
+  
+  # Sidebar 
+  sidebarLayout(
+    sidebarPanel(
+      # select continents
+      selectInput(inputId = "filter_country", 
+                  label = "Filter by Country (multiple):",
+                  choices = 
+                    list("North America" = c("United States of America", 
+                                              "Canada", "Belize",
+                                             "Costa Rica", "Cuba" , 
+                                             "Dominican Republic", "El Salvador", 
+                                             "Greenland" , "Guatemala" , 
+                                             "Haiti" , "Honduras" , 
+                                             "Jamaica" , "Mexico" , 
+                                             "Nicaragua" , "Panama" , 
+                                             "Puerto Rico" , "The Bahamas" , 
+                                             "Trinidad and Tobago" ), 
+                         "South America" = c("Argentina", "Bolivia", "Brazil", 
+                                             "Chile", "Colombia", "Ecuador", 
+                                             "Falkland Islands", "Guyana", 
+                                             "Paraguay", "Peru", "Suriname", 
+                                             "Uruguay", "Venezuela"), 
+                         "Africa" = c("Algeria", "Angola", "Benin", "Botswana", 
+                                      "Burkina Faso", "Burundi", "Cameroon", 
+                                      "Central African Republic", "Chad", 
+                                      "Democratic Republic of the Congo", "Djibouti", 
+                                      "Egypt", "Equatorial Guinea", "Eritrea", "eSwatini", 
+                                      "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", 
+                                      "Guinea-Bissau", "Ivory Coast", "Kenya", "Lesotho",
+                                      "Liberia", "Libya", "Madagascar", "Malawi", "Mali", 
+                                      "Mauritania", "Morocco", "Mozambique", "Namibia", 
+                                      "Niger", "Nigeria", "Republic of the Congo",
+                                      "Rwanda", "Senegal", "Sierra Leone", "Somalia", 
+                                      "Somaliland", "South Africa", "South Sudan", "Sudan", 
+                                      "Tanzania", "Togo", "Tunisia", "Uganda", "Western Sahara", 
+                                      "Zambia", "Zimbabwe"), 
+                         "Asia" = c("Afghanistan", "Armenia", "Azerbaijan", "Bangladesh", "Bhutan", "Brunei", "Cambodia", "China", "Cyprus", "East Timor", "Georgia", "India", "Indonesia", "Iran", "Iraq", "Israel", "Japan", "Jordan", "Kazakhstan", "Kuwait", "Kyrgyzstan", "Laos", "Lebanon", "Malaysia", "Mongolia", "Myanmar", "Nepal", "North Korea", "Northern Cyprus", "Oman", "Pakistan", "Palestine", "Philippines", "Qatar", "Saudi Arabia", "South Korea", "Sri Lanka", "Syria", "Taiwan", "Tajikistan", "Thailand", "Turkey", "Turkmenistan", "United Arab Emirates", "Uzbekistan", "Vietnam", "Yemen"), 
+                         "Europe" = c("Albania", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Czechia", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kosovo", "Latvia", "Lithuania", "Luxembourg", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Norway", "Poland", "Portugal", "Republic of Serbia", "Romania", "Russia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine", "United Kingdom"), 
+                         "Antarctica" = c("Antarctica"), 
+                         "Oceania" = c("Australia", "Fiji", "New Caledonia", "New Zealand", "Papua New Guinea", "Solomon Islands", "Vanuatu")), 
+                  selected = NULL, 
+                  multiple = T, 
+                  selectize = T, 
+                  width = NULL, 
+                  size = NULL),
+      tableOutput("data")
+      
+    ),
+    mainPanel(
+      plotOutput(outputId = "world_map")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-   
+  data("countries110")
+  
+  output$world_map <- renderPlot({
+    ggplot() + 
+      geom_sf(data = countries110[countries110$geounit %in% input$filter_country,], 
+              aes(fill = continent), color = "black")+
+      theme(panel.background = element_rect(fill = "#9ce4ff"), 
+            legend.position = "none")
+  })
 }
 
 # Run the application 
