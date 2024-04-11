4d51f65afb85090850646154482eff1cf357e42f
Tim Bender <bendertj@gmail.com>
2024-04-11 18:24:51 -0400

    swephr stuff

--- R/swephR_basics.R
+++ R/swephR_basics.R
@@ -4,15 +4,19 @@ library(renv)
 library(swephR)
 library(lubridate)
 library(dplyr)
-library(tigris)
-library(censusxy)
-library(scales)
-library(ggplot2)
-library(sf)
-library(glue)
+#library(tigris)
+#library(censusxy)
+#library(scales)
+#library(ggplot2)
+#library(sf)
+#library(glue)
 #library(rsconnect)
 #library(qrcode)
-library(rnaturalearthdata) # for map of world
+#library(rnaturalearthdata) # for map of world
+library(leaflet)
+library(geoloc)
+#library(remotes)
+#remotes::install_github("ColinFay/geoloc")
 
 #renv::snapshot()
 
@@ -33,24 +37,23 @@ loctz.in   <- "America/New_York"
 # Transformations----
 greg_utcdt.in <- greg_dt.in |> with_tz(tzone = "UTC")
 
-jul_utcdt.in  <- swephR::swe_utc_to_jd(year = year(greg_utcdt.in), 
-                                    month = lubridate::month(greg_utcdt.in), 
-                                    day   = mday(greg_utcdt.in), 
-                                    houri = hour(greg_utcdt.in), 
-                                    min   = minute(greg_utcdt.in), 
-                                    sec   = second(greg_utcdt.in), 
-                                    gregflag = 1 #SE$GREG_CAL = 1
-                                    )$dret[2] # dret[2] = UTC, [1] = ET (not sure what et means)
+jul_utcdt.in  <- swephR::swe_utc_to_jd(year     = year(greg_utcdt.in), 
+                                       month    = lubridate::month(greg_utcdt.in), 
+                                       day      = mday(greg_utcdt.in), 
+                                       houri    = hour(greg_utcdt.in), 
+                                       min      = minute(greg_utcdt.in), 
+                                       sec      = second(greg_utcdt.in), 
+                                       gregflag = 1)$dret[2] # dret[2] = UTC, [1] = ET (not sure what et means)
 
 
 # SWEPHR FUNS----
 # find the next eclipse for a given geographic position;
-when_next <- swe_sol_eclipse_when_loc(jd_start  = jul_utcdt.in, 
-                         ephe_flag = 4, 
-                         geopos    = c(x = lon.in, 
-                                       y = lat.in, 
-                                       z = 10), 
-                         backward  = FALSE) 
+when_next.sol <- swe_sol_eclipse_when_loc(jd_start  = jul_utcdt.in, 
+                                      ephe_flag = 4, 
+                                      geopos    = c(x = lon.in, 
+                                                    y = lat.in, 
+                                                    z = 10), 
+                                      backward  = FALSE) 
 
 # find the next eclipse globally (including type of eclipse);
 ecl_total   <- swe_sol_eclipse_when_glob(jd_start  = jul_utcdt.in,
@@ -71,21 +74,23 @@ ecl_hybrid  <- swe_sol_eclipse_when_glob(jd_start  = jul_utcdt.in,
                                          backward  = FALSE)
 
 ecl_type <- c("Total Eclipse", "Annular", 
-              "Partial", "Hybrid")[which(abs(c(ecl_total$tret[2] - when_next$tret[2],
-                                               ecl_annular$tret[2] - when_next$tret[2],
-                                               ecl_partial$tret[2] - when_next$tret[2],
-                                               ecl_hybrid$tret[2] - when_next$tret[2])) == 
-                                           min(abs(c(ecl_total$tret[2] - when_next$tret[2],
-                                                     ecl_annular$tret[2] - when_next$tret[2],
-                                                     ecl_partial$tret[2] - when_next$tret[2],
-                                                     ecl_hybrid$tret[2] - when_next$tret[2]))))]
+              "Partial", "Hybrid")[which(abs(c(ecl_total$tret[2] - when_next.sol$tret[2],
+                                               ecl_annular$tret[2] - when_next.sol$tret[2],
+                                               ecl_partial$tret[2] - when_next.sol$tret[2],
+                                               ecl_hybrid$tret[2] - when_next.sol$tret[2])) == 
+                                           min(abs(c(ecl_total$tret[2] - when_next.sol$tret[2],
+                                                     ecl_annular$tret[2] - when_next.sol$tret[2],
+                                                     ecl_partial$tret[2] - when_next.sol$tret[2],
+                                                     ecl_hybrid$tret[2] - when_next.sol$tret[2]))))]
+
+
 
 # compute the geographic location of a solar eclipse for a given tjd;
 
 jul_utc_ecl.begin <- swe_sol_eclipse_when_glob(jd_start  = jul_utcdt.in,
-                                             ephe_flag = 4, 
-                                             ifltype   = 0, # 0 = any eclipse 
-                                             backward  = FALSE)$tret[c(3:4)]
+                                               ephe_flag = 4, 
+                                               ifltype   = 0, # 0 = any eclipse 
+                                               backward  = FALSE)$tret[c(3:4)]
 
 
 swe_sol_eclipse_where(jd_ut     = min(jul_utc_ecl.begin), 
